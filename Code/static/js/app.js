dd517d7bcd6695bbc16447078c304a821afd21d1
Dustin Elery <delery7@yahoo.com>
2021-03-25 21:02:11 -0400

    Have the bar and bubble chart functioning

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,27 +1,57 @@
 // python -m http.server
-// var sampleValues = data.samples;
-// console.log(sampleValues);
+
+// function unpack(rows, index) {
+//  return rows.map(function(row) {
+//    return row[index];
+//  });
+// }
 
 function buildPlot() {
 
   d3.json("samples.json").then(function(data) {
     
-    var sampleData = data ;// sampleData=data;
-    var sampleValues = data.samples;
-    var labels1=data.otu_ids
-    var hover1 = data.otu_labels
-    // console.log(sampleValues);
+    var sampleValues = data.samples[0].sample_values;
+    var otu_ids = data.samples[0].otu_ids;
+    var otu_labels =data.samples[0].otu_labels;;
 
     var trace1 = {
-      type: "box",
-    //   mode: "lines",
-      name: "Sample Data",
-      text: hover1,
-      x: sampleValues,
-      y: labels1,
+      type: "bar",
+      text: otu_labels,
+      x: otu_ids,
+      y: sampleValues,
     };
+
+    var trace2 = {
+        x: otu_ids,
+        y: sampleValues,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+            color: otu_ids,
+            // opacity: [1, 0.8, 0.6, 0.4],
+            size: sampleValues
+          }
+      };
+
     var data = [trace1];
-    Plotly.newPlot("bar", data);//, layout);
+    var dataBubble = [trace2];
+
+    var layout = {
+        title: "Sample Data",
+        barmode: "group",
+        height: 600,
+        width: 1200
+      };
+
+    var layoutBubble ={
+        title: 'Bubble Chart',
+        showlegend: false,
+        height: 600,
+        width: 1200
+    }
+
+    Plotly.newPlot("bar", data, layout);
+    Plotly.newPlot('bubble', dataBubble, layoutBubble);
     });
 };
 
